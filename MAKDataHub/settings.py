db1149ffa3293bcb2ac343ed2d7f38c80d1653b8
Wawrzeńczyk Adam <adam.wawrzenczyk@gmail.com>
2019-11-20 16:37:57 +0100

    Logging settings uploaded

--- MAKDataHub/settings.py
+++ MAKDataHub/settings.py
@@ -126,3 +126,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{asctime}|{levelname}|{module}|{message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/my_debug.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
