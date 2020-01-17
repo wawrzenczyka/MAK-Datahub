6db988a1088d7db2880b465755521b2a9e8cdb1e
adamw00000 <adam.wawrzenczyk@gmail.com>
2020-01-17 14:53:35 +0100

    Removed browser login

--- api/urls.py
+++ api/urls.py
@@ -17,7 +17,6 @@ router.register(r'profiles', ProfileInfoViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/register/', include('rest_auth.registration.urls')),
     url(r'devices/(?P<id>\w+)/profiles/latest', LatestDeviceProfileInfo.as_view()),
