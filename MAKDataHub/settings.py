230e51dc54c45922968994d484f995a171954207
Wawrzeńczyk Adam <adam.wawrzenczyk@gmail.com>
2020-01-17 10:31:02 +0100

    Pythonanywhere DB restored

--- MAKDataHub/settings.py
+++ MAKDataHub/settings.py
@@ -90,17 +90,17 @@ WSGI_APPLICATION = 'MAKDataHub.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'makengineering$mak-db-new-schema',
-    #     'USER': 'makengineering',
-    #     'PASSWORD': 'admin123$',
-    #     'HOST': 'makengineering.mysql.pythonanywhere-services.com',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'makengineering$mak-db-new-schema',
+        'USER': 'makengineering',
+        'PASSWORD': 'admin123$',
+        'HOST': 'makengineering.mysql.pythonanywhere-services.com',
+    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'makdb',
