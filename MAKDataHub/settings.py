8eedf259a72ead1b1afaedac0f53cd0e71cc818a
adamw00000 <adam.wawrzenczyk@gmail.com>
2020-01-20 21:07:49 +0100

    Email backend setting

--- MAKDataHub/settings.py
+++ MAKDataHub/settings.py
@@ -199,6 +199,8 @@ LOGGING = {
     },
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
