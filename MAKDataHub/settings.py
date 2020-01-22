97586b92f9d6e1d4adc2528439ce9db5a6e60b60
Wawrzeńczyk Adam <wawrzenczyka@student.mini.pw.edu.pl>
2020-01-22 15:51:58 +0000

    Local storage used

--- MAKDataHub/settings.py
+++ MAKDataHub/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # valid values ['LOCAL', 'AWS_S3', 'GOOGLE_DRIVE', 'DROPBOX']
-DATAHUB_STORAGE = 'DROPBOX' 
+DATAHUB_STORAGE = 'LOCAL' 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
